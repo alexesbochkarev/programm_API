91ac9da40662e934fd567ef71937244a00fc90cb
alexes <alexes.bochkarev@mail.ru>
2023-04-21 11:10:08 +0400

    settings local

--- programms/settings.py
+++ programms/settings.py
@@ -80,22 +80,22 @@ WSGI_APPLICATION = 'programms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#         'NAME': 'programm',
-#         'USER': 'programm_user',
-#         'PASSWORD': '123456789',
-#     },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'PORT': '',
+        'NAME': 'programm',
+        'USER': 'programm_user',
+        'PASSWORD': '123456789',
+    },
+}
 
 
 # Password validation
